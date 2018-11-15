a871da89782413cfdfb25fe0e7580c414142665f
oscarcillo <oscar.tutor.rubio@gmail.com>
2018-11-15 19:07:00 +0100

    Clase que leer caracteres de 5 ficheros con hilos.

--- Procesos_Java/src/P02_Hilos_Java/A03_LeerFicherosHilos.java
+++ Procesos_Java/src/P02_Hilos_Java/A03_LeerFicherosHilos.java
@@ -1,6 +1,7 @@
 package P02_Hilos_Java;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class A03_LeerFicherosHilos implements Runnable{
@@ -13,26 +14,46 @@ public class A03_LeerFicherosHilos implements Runnable{
 	public void run()
 	{
 		n_caracteres = 0;
-		
-		ruta = "src\\P02_Hilos_Java\\ficheros\\fichero" + i + ".txt";
-		File f = new File(ruta);
-		FileReader fileReader = new FileReader(f);
-		
-		while(fileReader.read()!=-1)//while para leer el fichero
+		//
+		try
 		{
-				n_caracteres++;
+			File f = new File(ruta);
+			FileReader fileReader = new FileReader(f);
+			
+			while(fileReader.read()!=-1)//while para leer el fichero
+			{
+					n_caracteres++;
+			}
+			
+			System.out.println("Numero de caracteres: " + n_caracteres);
+			
+			fileReader.close();
 		}
+		catch(Exception e){}
 		
-		System.out.println("Numero de caracteres: " + n_caracteres);
+		//
 		
-		fileReader.close();
 	}
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 
-
-
+		t_inicio = System.currentTimeMillis(); // tiempo en milisegundos
+		
+		for(int i = 1; i <= 5; i++)
+		{
+			A03_LeerFicherosHilos h = new A03_LeerFicherosHilos();
+			ruta = "src\\P02_Hilos_Java\\ficheros\\fichero" + i + ".txt";
+			Thread t = new Thread(h);
+			t.start();
+		}
+		
+		t_fin = System.currentTimeMillis();
+		
+		long tiempo_total = t_fin - t_inicio;
+		
+		System.out.println("Tiempo transcurrido: " + tiempo_total);
+		
 	}
 
 }
