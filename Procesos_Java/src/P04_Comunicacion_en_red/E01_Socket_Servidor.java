5180bfeaaa15576a1cd35e79bb0ed558d0c8fcdb
oscarcillo <oscar.tutor.rubio@gmail.com>
2018-12-13 18:43:00 +0100

    Clases ejemplo de conexion entre sockets

--- Procesos_Java/src/P04_Comunicacion_en_red/E01_Socket_Servidor.java
+++ Procesos_Java/src/P04_Comunicacion_en_red/E01_Socket_Servidor.java
@@ -1,10 +1,46 @@
 package P04_Comunicacion_en_red;
 
-public class E01_Socket_Servidor {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/**
+ * Clase que se conecta con el cliente por medio de sockets
+ * @author ifc
+ *
+ */
+public class E01_Socket_Servidor{
+	
+	static Scanner teclado;
 
+	public static void main(String[] args) throws IOException {
+		//VARIABLES
+		teclado = new Scanner(System.in);
+		String texto = "";
+		char c;
+		
+		//creación del socket con el puerto por le que va a escuchar
+		ServerSocket serversocket = new ServerSocket(9999);
+		
+		//recibir el socket cliente
+		System.out.println(serversocket.getLocalPort());
+		Socket socketcliente = serversocket.accept();
+		
+		//cerramos el socket servidor
+		serversocket.close();
+		
+		//Creamos InputStream para recibir datos del socket cliente
+		//y leemos el texto que tiene
+		InputStream is = socketcliente.getInputStream();
+		
+		while((c = (char) is.read())!=-1)
+			texto = texto + c;
+			
+		System.out.println("Texto recibido: " + texto);
+		
+		socketcliente.close();
 	}
-
 }
