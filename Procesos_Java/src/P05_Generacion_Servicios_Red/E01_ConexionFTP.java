08f29206fb349812502868454727368cb841ac6a
oscarcillo <oscar.tutor.rubio@gmail.com>
2019-01-15 18:45:49 +0100

    Ejemplos de conexion a servidor FTP con Java

---
+++ Procesos_Java/src/P05_Generacion_Servicios_Red/E01_ConexionFTP.java
@@ -0,0 +1,45 @@
+package P05_Generacion_Servicios_Red;
+
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.*;
+/*
+ https://sites.google.com/site/nikos3194rfcindex/home/old-school/ftp
+ ftp.uv.es
+ ftp.unavarra.es
+ ftp.rediris.es
+ ftp.uma.es
+ ftp.udc.es
+ ftp.dit.upm.es
+ ftp.freenet.de
+ 
+ */
+
+public class E01_ConexionFTP {
+	public static void main(String[] args) throws SocketException, IOException {
+
+		FTPClient cliente = new FTPClient();
+		String servFTP = "ftp.rediris.es"; // servidor FTP
+		System.out.println("Nos conectamos a: " + servFTP);
+		cliente.connect(servFTP);
+
+		// respuesta del servidor FTP
+		System.out.print(cliente.getReplyString());
+		// código de respuesta
+		int respuesta = cliente.getReplyCode();
+
+		System.out.println("Respuesta: "+respuesta);
+		
+		// comprobación del código de respuesta
+		if (!FTPReply.isPositiveCompletion(respuesta)) {
+			cliente.disconnect();
+			System.out.println("Conexión rechazada: " + respuesta);
+			System.exit(0);
+		}
+		// desconexión del servidor FTP
+		cliente.disconnect();
+		System.out.println("Conexión finalizada.");
+	}
+
+}//
